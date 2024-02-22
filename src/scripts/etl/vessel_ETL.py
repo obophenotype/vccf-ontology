1e9525985fb0fd33a2125bccc7be54407ffb9d71
Anita Caron <anitac@ebi.ac.uk>
2024-02-22 15:04:07 -0300

    Updated vessel and vessel organ crosswalk (#5)
    * draft add vessel organ crosswalk
    
    * update vessel_organ_crosswalk etl script
    
    * update script to transform crosswalk into robot template
    
    * update_repo
    
    * update vessel_organ_crosswalk component
    
    * update ro and uberon imports
    
    * add `directly_supplies_drains` temp definition
    
    * apply flake8 black formatting
    
    * update imports
    
    * remove temporary RO property
    
    * update vessels and vessel organ crosswalk
    
    * update python script path
    
    * fix path to python script
    
    * Create vessel_ETL.py in etl directory
    
    * Create utils.py in etl directory
    
    * Rename vessel_organ_crosswalk_ETL.py to vessel_organ_crosswalk_ETL.py
    
    * Delete src/scripts/ETL/utils.py
    
    * Delete src/scripts/ETL/vessel_ETL.py
    
    * fix typo in scripts folder name
    
    ---------
    
    Co-authored-by: Anita Caron <anitacaron@users.noreply.github.com>

--- src/scripts/etl/vessel_ETL.py
+++ src/scripts/etl/vessel_ETL.py
@@ -1,17 +1,9 @@
-import pandas as pd
 import argparse
-from pathlib import Path
 
-def generate_id(start_range, end_range):
-    for i in range(start_range, end_range, 1):
-        yield str(i)
+import pandas as pd
+import numpy as np
+from utils import extract, generate_id, load, read, search_id, transform_id
 
-def transform_id(term):
-    split = term.lower().split('a')
-    return f'FMA:{split[1]}'
-
-def extract(data: pd.DataFrame, columns_extract: list) -> pd.DataFrame:
-    return data[columns_extract]
 
 def transform_pattern(data: pd.DataFrame) -> pd.DataFrame:
     # columns_pattern = ["defined_class", "label", "human_label", "parent", "location", "xrefs", "synonym", "synonym_xrefs", "taxon"]
@@ -23,19 +15,20 @@ def transform_pattern(data: pd.DataFrame) -> pd.DataFrame:
         r["defined_class"] = f'VCCF:{next(vccf_id)}'
         r["label"] = row['VesselBaseName'].rstrip()
         r["human_label"] = f'{row["VesselBaseName"].rstrip()} (Human)'
-        
+
         if not pd.isna(row['UBERON']):
             r['parent'] = row['UBERON']
-        elif not 'fma' in row['VesselTypeID']:
+        elif 'fma' not in row['VesselTypeID']:
             r['parent'] = row['VesselTypeID']
 
         r['location'] = row['BodyPart']
 
         references = []
-        if 'http' in row['ReferenceURL'] and not 'UBERON' in row['ReferenceURL']:        
-            references.append(row['ReferenceURL'])
-        if not pd.isna(row['ReferenceDOI']):
-            references.append(f'DOI:{row["ReferenceDOI"]}')
+        if str(row['ReferenceURL']) != 'nan':
+            if 'http' in row['ReferenceURL'] and 'UBERON' not in row['ReferenceURL']:
+                references.append(row['ReferenceURL'])
+            if not pd.isna(row['ReferenceDOI']):
+                references.append(f'DOI:{row["ReferenceDOI"]}')
 
         r['xrefs'] = '|'.join(references)
 
@@ -47,30 +40,28 @@ def transform_pattern(data: pd.DataFrame) -> pd.DataFrame:
 
     return pd.DataFrame.from_records(data_pattern)
 
-def search_id(data, term_label):
-    filter = data[data["label"] == term_label.strip()]
-    return filter['defined_class'].values[0]
 
-def transform_template(pattern_data: pd.DataFrame, template_data: pd.DataFrame) -> pd.DataFrame:
-    data_template = [{"Vessel": "ID" , "branches from": "SC 'connected to' some %"}]
+def transform_template(
+    pattern_data: pd.DataFrame,
+    template_data: pd.DataFrame
+) -> pd.DataFrame:
+    data_template = [{"Vessel": "ID", "branches from": "SC 'connecting branch of' some %"}]
     for _, row in template_data.iterrows():
         r = {}
         r['Vessel'] = search_id(pattern_data, row['VesselBaseName'])
         r['branches from'] = search_id(pattern_data, row['BranchesFrom'])
         data_template.append(r)
-    
-    return pd.DataFrame.from_records(data_template)
-    
 
-def load(data: pd.DataFrame, file: Path):
-    data.to_csv(file, sep='\t', index=False)
-    
+    return pd.DataFrame.from_records(data_template)
+
 
 def main(args):
-    data = pd.read_csv(args.input, encoding="latin-1")
-    pattern_data = extract(data=data, columns_extract=["VesselBaseName", "VesselType", "VesselTypeID", "ReferenceURL", "ReferenceDOI",
-                       "BodyPart", "BodyPartID", "UBERON", "UBERONLabel", "FMALabel", "FMA"
-                      ])
+    data = read(args.input)
+    pattern_data = extract(data=data, columns_extract=[
+        "VesselBaseName", "VesselType", "VesselTypeID", "ReferenceURL", "ReferenceDOI",
+        "BodyPart", "BodyPartID", "UBERON", "UBERONLabel", "FMALabel", "FMA"
+        ]
+                           )
     pattern_data = transform_pattern(pattern_data)
     load(pattern_data, args.pattern)
 
@@ -80,10 +71,10 @@ def main(args):
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--input', help='vessel input file')
-  parser.add_argument('--pattern', help='dos-dp pattern output file')
-  parser.add_argument('--template', help='robot template output file')
-  
-  args = parser.parse_args()
-  main(args)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', help='vessel input file')
+    parser.add_argument('--pattern', help='dos-dp pattern output file')
+    parser.add_argument('--template', help='robot template output file')
+
+    args = parser.parse_args()
+    main(args)
---
+++ src/scripts/etl/vessel_organ_crosswalk_ETL.py
@@ -0,0 +1,73 @@
+"""
+Script to transform vesse_organ_crosswalk CSV file into ROBOT templ
+"""
+from argparse import ArgumentParser, Namespace
+
+import pandas as pd
+from utils import extract, generate_id, load, read, search_id
+
+
+def crosswalk_template(crosswalk_data, vessels_data) -> pd.DataFrame:
+    template = [
+        {
+            "Vessel": "ID",
+            "TYPE": "TYPE",
+            "Label": "LABEL",
+            "overlaps": "SC overlaps some %",
+            "part_of": "SC 'part of' some %",
+            "directly_supplies_drains": "SC 'directly supplies and drains' some %",
+            "connected_to": "SC 'connected to' some %",
+            "drains": "SC 'vessel drains blood from' some %",
+            "supplies": "SC 'vessel supplies blood to' some %",
+            "located_in": "SC 'located in' some %",
+        }
+    ]
+
+    vccf_id = generate_id(2000000, 2999999)
+    new_vessels = {}
+    for _, row in crosswalk_data.iterrows():
+        if str(row["Relationship"]) != "nan":
+            vessel_id = search_id(vessels_data, row["Vessel"])
+
+            if "NOTFOUND" in vessel_id:
+                if row["Vessel"] not in new_vessels:
+                    new = next(vccf_id)
+                    new_vessels[row["Vessel"]] = new
+                    vessel_id = f"VCCF:{new}"
+                else:
+                    vessel_id = f"VCCF:{new_vessels[row['Vessel']]}"
+            r = {}
+            r["Vessel"] = vessel_id
+            if row["Vessel"] in new_vessels:
+                r["Label"] = row["Vessel"] 
+            r[f"{row['Relationship']}"] = row["BodySubPartID"] if row["BodySubPartID"] else next(vccf_id)
+            template.append(r)
+
+    return pd.DataFrame.from_records(template)
+
+
+def main(args: Namespace):
+    data = read(args.input)
+    vessels_data = read(args.vessels, sep="\t")
+    crosswalk_data = extract(data=data, columns_extract=[
+        "Vessel",
+        "Relationship",
+        "BodyPartID",
+        "BodySubPart",
+        "BodySubPartID"
+    ])
+
+    template = crosswalk_template(
+        crosswalk_data=crosswalk_data,
+        vessels_data=vessels_data
+    )
+    load(template, args.template)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--input', help="vessel - organ crosswalk input file")
+    parser.add_argument('--template', help="robot template output file")
+    parser.add_argument('--vessels', help="csv file listing vessels term id")
+
+    args = parser.parse_args()
+    main(args)
